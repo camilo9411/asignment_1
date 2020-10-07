75dadf691a8bf436cb820a52ad0f68c25aa20840
camilo9411 <58343811+camilo9411@users.noreply.github.com>
2020-10-07 13:41:33 -0400

    commit on master

--- asignment_01.py
+++ asignment_01.py
@@ -5,6 +5,7 @@ def main():
     myList = []
     choise = ' '
     while (choise != 'Q'):
+        system('cls')
         print('\t-----MENU----')
         print('P - Print numbers')
         print('A - Add a number')
@@ -16,12 +17,14 @@ def main():
         choise = input('Enter your Choice: ').upper()
 
         if choise == 'P':
+            system('cls')
             print('\t----DISPLAY----')
             if not myList:
                 print('[] - the list is empty')
             else:
                 print(str(myList).replace(",", ""))
         elif choise == 'A':
+            system('cls')
             print('\t----ADDING----')
             while True:
                 num = input('Enter a integer to add in the list: ')
@@ -33,6 +36,7 @@ def main():
             myList.append(int(num))
             print('Number {0} added.'.format(num))
         elif choise == 'M':
+            system('cls')
             print('\t----MEAN OF THE LIST----')
             if myList:
                 mean_list = sum(myList) / len(myList)
@@ -40,12 +44,14 @@ def main():
             else:
                 print('Unable to calculate the mean - no data')
         elif choise == 'S':
+            system('cls')
             print('\t----SMALLEST NUMBER----')
             if myList:
                 print('The smallest number is', min(myList))
             else:
                 print('Unable to determine the smallest number - list is empty')
         elif choise == 'L':
+            system('cls')
             print('\t----LARGEST NUMBER----')
             if myList:
                 print('The largest number is', max(myList))
@@ -53,6 +59,7 @@ def main():
                 print('Unable to determine the largest number - list is empty')
         elif choise == 'F':
             while True:
+                system('cls')
                 print('\t----FIND A NUMBER----')
                 num_to_find = input('Enter the number you want to find: ')
                 try:
@@ -60,14 +67,17 @@ def main():
                     break
                 except ValueError:
                     print('Not valid input...Try again')
+                    input('Press any key to continue...')
             if int(num_to_find) in myList:
                 print('The number ' + str(num_to_find) + ' is in the list ' + str(myList.count(num_to_find)) + ' times.')
             else:
                 print('The number ' + str(num_to_find) +' is not in the list')
         elif choise == 'Q':
+            system('cls')
             print('Goodbye')
             break
         else:
+            system('cls')
             print('Unknown selection, please try again')
         input('Press any key to continue...')
 
